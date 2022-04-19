390d88c1635bf0ca9c2512c50eb55edb776c688a
AYLIEN <mahyar@aylien.com>
2022-04-19 11:05:56 +0430

    chore(Home): wrap Home page with HTMLHeadWrapper

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import type {FC} from 'react';
 
 import Container from '@/components/Container';
+import HTMLHeadWrapper from '@/components/HTMLHeadWrapper';
 import Layout from '@/layout';
 import QuestionsList from '@/widget/Home/List';
 import ManipulateQuestionModal from '@/widget/Home/ManipulateModal';
 
 const Home: FC<Record<string, unknown>> = () => {
   return (
-    <Layout>
-      <Container className="py-5 px-2">
-        <QuestionsList />
-        <ManipulateQuestionModal />
-      </Container>
-    </Layout>
+    <HTMLHeadWrapper title="Studocu | QA App" description="Question and answer app">
+      <Layout>
+        <Container className="py-5 px-2">
+          <QuestionsList />
+          <ManipulateQuestionModal />
+        </Container>
+      </Layout>
+    </HTMLHeadWrapper>
   );
 };
 
